cf17605a4d8095d622b284e87864e40cd72ec6f2
Jardel Knirsch <jardel.knirsch@meta.com.br>
2019-04-10 15:50:27 -0300

    Primeira parte do treinamento

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h4>{{ comic.title }}</h4>
+        <img height="300" :src="imageUrl" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+export default {
+    name: 'Card',
+
+    computed: {
+        imageUrl() {
+            return this.comic.thumbnail.path + '.' + this.comic.thumbnail.extension
+        },
+    },
+
+    props: {
+        comic: {
+            required: true,
+            default: () => {}
+        }
+    }
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,17 +3,32 @@
     <h1>
       Hello World!
     </h1>
+
+    <template v-for="(comic, index) of comics">
+      <card v-bind:key="index" :comic="comic" />
+    </template>
   </div>
 </template>
 
 <script>
 import Connections from '../mixins/connections'
+import Card from './Card'
 
 export default {
   name: 'HelloWorld',
 
   props: {
     msg: String,
+  },
+
+  components: {
+    Card,
+  },
+
+  data() {
+    return {
+      comics: [],
+    }
   },
 
   mixins: [
@@ -23,6 +38,7 @@ export default {
   mounted() {
     this.$http.get(`https://gateway.marvel.com/v1/public/comics?${this.hash}`)
       .then(({ data: { data: { results: response } } }) => {
+        this.comics = response;
         console.log(response);
       })
       .catch(e => {
